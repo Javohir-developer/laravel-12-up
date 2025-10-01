148d3b5f366e4205ae60b3ddb6e8e083f6de9e3a
MacBook Pro <macbookpro@MacBookPro.bbrouter>
2025-10-01 19:06:49 +0500

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,27 @@
-import '@/bootstrap';
-import { createApp } from 'vue';
-import App from '@/layouts/main/main.vue';
-import router from '@/router';
+import '../css/app.css';
+import './bootstrap';
 
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createApp, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue'),
+        ),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
