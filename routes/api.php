e0714b8a32d827102bc64fe8628d893fa03dcb8a
Javohir-developer <elmurodovdeveloper@gmail.com>
2026-03-04 20:22:45 +0500

    updates

--- routes/api.php
+++ routes/api.php
@@ -12,12 +12,7 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/test', [MovieController::class, 'search'])->name('movies.search');
 Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
-<<<<<<< HEAD
 Route::post('/create-movies', [MovieController::class, 'createMovies'])->name('movies.create');
 Route::post('/edit-movies', [MovieController::class, 'editMovies'])->name('movies.edit');
 Route::get('/get-status', [MovieController::class, 'getStatus'])->name('movies.status');
 Route::get('/get-movie-for-edit/{id}', [MovieController::class, 'getMovieForEdit'])->name('movies.getMovieForEdit');
-=======
-Route::post('/create-movies', [MovieController::class, 'createMovies'])->name('create.movies');
-Route::get('/get-status', [MovieController::class, 'getStatus'])->name('get.status');
->>>>>>> f946413cffde185a4b24753e1313de5c7e61d215
